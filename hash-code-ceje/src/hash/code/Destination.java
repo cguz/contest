7054a9a4f7b466c37870b3d9632ed47a4f4da9b6
Cguzman <developer@cguz.org>
2016-01-29 17:40:03 +0100

    Comparable

--- hash-code-ceje/src/hash/code/Destination.java
+++ hash-code-ceje/src/hash/code/Destination.java
@@ -1,11 +1,17 @@
 package hash.code;
 
-public class Destination {
+public class Destination implements Comparable<Destination> {
 
 	int origin = -1;
 	int destination = -1;
 	double time = 0;
 	double length = 0;
 	int numberTimes = 0;
+	
+	
+	@Override
+	public int compareTo(Destination o) {
+		return 0;
+	}
 	
 }
---
+++ hash-code-ceje/src/hash/code/QueueDestiny.java
@@ -0,0 +1,15 @@
+package hash.code;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueDestiny{
+
+	public Queue<Destination> colaEspera;
+
+	public QueueDestiny(){
+
+		colaEspera = new PriorityQueue<Destination>();
+
+	}
+
+}
