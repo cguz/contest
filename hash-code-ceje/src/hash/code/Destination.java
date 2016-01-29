a133591eeeb5617f92ded1d385abe1bb643717fd
jesus <jesus@RED-QUEEN>
2016-01-29 17:53:13 +0100

    Destination implement compareTo

--- hash-code-ceje/src/hash/code/Destination.java
+++ hash-code-ceje/src/hash/code/Destination.java
@@ -8,10 +8,29 @@ public class Destination implements Comparable<Destination> {
 	double length = 0;
 	int numberTimes = 0;
 	
-	
+	//this < o -> 	-1
+	//o > this ->	1
+	//this = 0
+	//	this < o	->  1
+	//	this > o	-> -1
+	//	this = o	0
 	@Override
 	public int compareTo(Destination o) {
+		if (this.length < o.length) {
+			return -1;
+		} else if (this.length > o.length) { 
+			return 1;
+		} else if (this.length == o.length) {
+			if(this.numberTimes < o.numberTimes) {
+				return 1;
+			} else if(this.numberTimes > o.numberTimes) {
+				return -1;
+			} else {
+				return 0;
+			}
+		}
+		
 		return 0;
 	}
-	
+
 }
