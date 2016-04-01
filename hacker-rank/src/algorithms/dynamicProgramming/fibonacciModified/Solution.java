4d539d7d5dda22322e3916379d391b7faa2c3d7c
Cesar <developer@cguz.org>
2016-04-01 21:15:59 +0200

    Hour rank

--- hacker-rank/src/algorithms/dynamicProgramming/fibonacciModified/Solution.java
+++ hacker-rank/src/algorithms/dynamicProgramming/fibonacciModified/Solution.java
@@ -1,30 +1,60 @@
 package algorithms.dynamicProgramming.fibonacciModified;
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Solution {
-
-    static BigInteger fibonaci(BigInteger a, BigInteger b, int c){
-        BigInteger T;
-   
-        for(int i=2;i<c;++i){
-            T = b;
-            b = b.pow(2).add(a);
-            a = T;
-        }
-        
-        return b;
-    }
     
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner in = new Scanner(System.in);
-        BigInteger a,b,N;
-        int c;
-        a =  in.nextBigInteger();
-        b =  in.nextBigInteger();
-        c = in.nextInt();
-        N = fibonaci(a, b, c);
-        System.out.println(N);
+    	int a =  in.nextInt();
+        
+        while(a < 0){
+	        in.nextInt(); 
+	        String[] N =  in.nextLine().split(" ");
+	        System.out.println(binarySearchSol(N));
+        }
     }
+
+	private static int binarySearchSol(String[] n) {
+		
+		int l = 0;
+		int h = n.length;
+		
+		int sol = 0;
+		while(l < h){
+			
+			boolean found = false;
+			
+			for(int i = l+1; i < h; ++i){
+				int sumL = sum(n, l, i);
+				int sumR = sum(n, i, h);
+				
+				if(sumL == sumR){
+					sol++;
+					if(Math.abs(l-i) > Math.abs(i-h)){
+						h = i;
+					}else{
+						l = i;
+					}
+					found = true;
+					break;
+				}
+			}
+			if(!found)
+				l = h;
+		}
+		
+		return sol;
+	}
+
+	private static int sum(String[] n, int from, int to) {
+		
+		int sum = 0;
+		
+		for(int i = from; i < to; i++){
+			sum += Integer.parseInt(n[i]);
+		}
+		
+		return sum;
+	}
 }
---
+++ hacker-rank/src/contests/hourrank7/NikitaAndTheGame/Solution.java
@@ -0,0 +1,30 @@
+package contests.hourrank7.NikitaAndTheGame;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Solution {
+
+    static BigInteger fibonaci(BigInteger a, BigInteger b, int c){
+        BigInteger T;
+   
+        for(int i=2;i<c;++i){
+            T = b;
+            b = b.pow(2).add(a);
+            a = T;
+        }
+        
+        return b;
+    }
+    
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        BigInteger a,b,N;
+        int c;
+        a =  in.nextBigInteger();
+        b =  in.nextBigInteger();
+        c = in.nextInt();
+        N = fibonaci(a, b, c);
+        System.out.println(N);
+    }
+}
